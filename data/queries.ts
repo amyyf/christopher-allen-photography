5a66380ffa0a17e98353c61f4666e39668c85daa
Amy Frieson <ava447@gmail.com>
2022-09-15 14:23:53 -0400

    Fix image query to not remake gallery query

--- data/queries.ts
+++ data/queries.ts
@@ -18,14 +18,7 @@ export const useImageQuery = (
   imageSlug: string | string[] | undefined,
   gallerySlug: string | string[] | undefined,
 ) => {
-  const { data: navData } = useSiteNavQuery();
-  const { data: galleryData } = useQuery(
-    ['gallery', gallerySlug],
-    () => getGalleryData(gallerySlug, navData),
-    {
-      enabled: !!navData && !!gallerySlug && typeof gallerySlug === 'string',
-    },
-  );
+  const { data: galleryData } = useGalleryQuery(gallerySlug);
   return useQuery(
     ['image', imageSlug],
     () => getImage(imageSlug, galleryData),
